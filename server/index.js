e48ee25faab84dea458a350dfe7130ebf6f0102b
Claire Dochez <clairedochez@gmail.com>
2023-07-06 04:51:38 +0200

    enabled cors

--- server/index.js
+++ server/index.js
@@ -5,3 +5,10 @@ const logger = require('./utils/logger')
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
 })
+
+// CORS Enabled
+app.all('/', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next()
+});
