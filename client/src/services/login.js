00c4691cbe0401802623a03ae747b03a04cc3f04
Claire Dochez <clairedochez@gmail.com>
2023-07-11 15:13:55 +0200

    build

--- client/src/services/login.js
+++ client/src/services/login.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:2121/api/login'
+const baseUrl = '/api/login'
 
 const login = async credentials => {
     const response = await axios.post(baseUrl, credentials)
